7d0378458aedc4bd38d45c227583e764a59f4596
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-02-02 20:54:11 +0000

    Updating options

--- src/api-client/ApiClientOptions.cs
+++ src/api-client/ApiClientOptions.cs
@@ -2,24 +2,19 @@
 {
     public class ApiClientOptions
     {
-        public string BaseUrl { get; }
+        public string BaseUrl { get; set; }
 
-        public string ApiKey { get; }
+        public string ApiKey { get; set; }
 
-        public string ApiAudience { get; }
+        public string ApiAudience { get; set; }
 
-        public string? ApiPathPrefix { get; }
+        public string? ApiPathPrefix { get; set; }
 
-        public ApiClientOptions(string baseUrl, string apiKey, string apiAudience)
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        public ApiClientOptions()
         {
-            BaseUrl = baseUrl;
-            ApiKey = apiKey;
-            ApiAudience = apiAudience;
-        }
 
-        public ApiClientOptions(string baseUrl, string apiKey, string apiAudience, string apiPathPrefix) : this(baseUrl, apiKey, apiAudience)
-        {
-            ApiPathPrefix = apiPathPrefix;
         }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     }
 }
