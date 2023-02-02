a7e9f33ba079a8ff58d010fc005c0f4fc10c38b8
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-02-02 19:07:50 +0000

    Rename extension, add additional

--- src/api-client/Extensions/ServiceCollectionExtensions.cs
+++ src/api-client/Extensions/ServiceCollectionExtensions.cs
@@ -4,9 +4,10 @@ namespace MxIO.ApiClient.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        public static void AddApiClientTokenProvider(this IServiceCollection serviceCollection)
+        public static void AddApiClient(this IServiceCollection serviceCollection)
         {
             serviceCollection.AddSingleton<IApiTokenProvider, ApiTokenProvider>();
+            serviceCollection.AddSingleton<IRestClientSingleton, RestClientSingleton>();
         }
     }
 }
